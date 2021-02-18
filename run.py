2ac3b74f2e8b9cb5d4dd15324d783beb1ea929b5
mlserver <johanmgina7@gmail.com>
2021-02-18 21:28:24 +0100

    quick test with less layers

--- run.py
+++ run.py
@@ -291,15 +291,15 @@ def getCNNClassifier(matList, emotionsList, datasetDividor=5, epochs=500, image_
         print("No model given, creating new model...")
         print("adding layers...")
         model = Sequential()
-        model.add(Conv2D(8,3,padding="same", activation="relu", input_shape=(image_shape[0], image_shape[1], 1)))
+        model.add(Conv2D(32,3,padding="same", activation="relu", input_shape=(image_shape[0], image_shape[1], 1)))
         model.add(MaxPool2D())
 
-        model.add(Conv2D(16, 3, padding="same", activation="relu"))
+        model.add(Conv2D(64, 3, padding="same", activation="relu"))
         model.add(MaxPool2D())
         model.add(Dropout(0.4))
 
         model.add(Flatten())
-        model.add(Dense(32,activation="relu"))
+        model.add(Dense(128,activation="relu"))
         model.add(Dense(7, activation="softmax"))
 
         model.summary()
